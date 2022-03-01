8bfa003c0199cd6cd6dd326d1977c915bf4af6ce
Jiho Lee <baleen37@gmail.com>
2022-03-01 11:35:01 +0900

    chore: init project

---
+++ demo/src/test/kotlin/com/wooto/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wooto.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
